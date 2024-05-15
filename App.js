0f2a5e6a015a3f6295b1118eb7bb7e8ec96ca4f0
nirgluzman <nir.gluzman@gmail.com>
2024-05-15 18:56:55 +0200

    add ImageBackground

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 
 // enable transitions between multiple colors in a linear direction.
 import { LinearGradient } from 'expo-linear-gradient';
@@ -8,11 +8,20 @@ import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <LinearGradient
-      colors={['#4e0329', '#ddb52f']} // array of colors that represent stops in the gradient.
-      style={styles.rootScreen}>
-      <StartGameScreen />
-    </LinearGradient>
+    <>
+      <StatusBar style='light' />
+      <LinearGradient
+        colors={['#4e0329', '#ddb52f']} // array of colors that represent stops in the gradient.
+        style={styles.rootScreen}>
+        <ImageBackground
+          source={require('./assets/images/background.png')}
+          resizeMode='cover' // the image keeps its aspect ratio and fills the given dimension.
+          imageStyle={{ opacity: 0.15 }} // the opacity of the image.
+          style={styles.rootScreen}>
+          <StartGameScreen />
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
