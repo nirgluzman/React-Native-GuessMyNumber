bf5998ff93745b8b942c4ec694d3674ae8945068
nirgluzman <nir.gluzman@gmail.com>
2024-05-16 11:46:30 +0200

    style enhance to StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import { Text, TextInput, View, StyleSheet, Alert } from 'react-native';
 
+import Title from '../components/ui/Title.js';
 import PrimaryButton from '../components/ui/PrimaryButton.js';
 import Colors from '../constants/colors.js';
 
@@ -32,22 +33,26 @@ function StartGameScreen({ onPickNumber }) {
   }
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.numberInput}
-        maxLength={2} // limits the maximum number of characters that can be entered.
-        keyboardType='number-pad' // determines which keyboard to open.
-        autoCapitalize='none' // determines whether to capitalize the first letter of a word (not releavnt for numeric values).
-        autoCorrect={false} // determines whether to automatically correct the input.
-        value={enteredNumber}
-        onChangeText={numberInputHandler}
-      />
-      <View style={styles.buttonsContainer}>
-        <View style={styles.buttonContainer}>
-          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-        </View>
-        <View style={styles.buttonContainer}>
-          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+    <View style={styles.rootContainer}>
+      <Title>Guess My Number</Title>
+      <View style={styles.inputContainer}>
+        <Text style={styles.instructionText}>Enter a Number</Text>
+        <TextInput
+          style={styles.numberInput}
+          maxLength={2} // limits the maximum number of characters that can be entered.
+          keyboardType='number-pad' // determines which keyboard to open.
+          autoCapitalize='none' // determines whether to capitalize the first letter of a word (not releavnt for numeric values).
+          autoCorrect={false} // determines whether to automatically correct the input.
+          value={enteredNumber}
+          onChangeText={numberInputHandler}
+        />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+          </View>
         </View>
       </View>
     </View>
@@ -57,10 +62,16 @@ function StartGameScreen({ onPickNumber }) {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1, // takes up all available space, as there are no sibling elements.
+    marginTop: 100,
+    alignItems: 'center'
+  },
+
   inputContainer: {
     justifyContent: 'center', // position elements along the main axis (default - column).
     alignItems: 'center', // position elements along the cross axis.
-    marginTop: 100,
+    marginTop: 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
@@ -72,6 +83,11 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25
+  },
+
+  instructionText: {
+    color: Colors.accent500,
+    fontSize: 24
   },
 
   numberInput: {
